0282fbfa4b775efb0c7b5e429a6983b869878732
the-chat <thechatllc@gmail.com>
2022-03-11 13:58:25 +0500

    firebase

---
+++ utils/firebase/index.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "@firebase/app"
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
+import { FIREBASE_PUBLIC_CONFIG } from "@the-chat/firebase"
+
+initializeApp(FIREBASE_PUBLIC_CONFIG)
+
+const auth = getAuth()
+auth.useDeviceLanguage()
+// learn
+auth.onAuthStateChanged(() => {})
+
+const db = getFirestore()
+
+const storage = getStorage()
+
+export { auth, db, storage }
