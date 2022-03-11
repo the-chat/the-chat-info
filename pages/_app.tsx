709d045924add9d9a35cff31f60d2f67d428e864
the-chat <thechatllc@gmail.com>
2022-03-11 13:58:10 +0500

    _app

---
+++ pages/_app.tsx
@@ -0,0 +1,43 @@
+import { appWithTranslation } from "next-i18next"
+import { useState } from "react"
+import { auth, db } from "utils/firebase"
+import { AppProps } from "next/app"
+import { Wrapper } from "@the-chat/ui-kit"
+import { INFO, SSO } from "@the-chat/config"
+import { useSidebarButtonsDefaultSortFn } from "@the-chat/ui-kit"
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <Wrapper
+    configProviderValue={{
+      auth,
+      sidebarOpen: useState<boolean>(false),
+      signOutArgs: [auth, "SIGN OUT", "ERROR"],
+      noUser: false,
+      useSidebarButtons: () => useSidebarButtonsDefaultSortFn({}),
+      InfoConfig: {
+        ...INFO,
+        HOST: INFO.DEFAULT_INFO_HOST,
+      },
+      SSOConfig: {
+        ...SSO,
+        HOST: SSO.DEFAULT_SSO_HOST,
+      },
+    }}
+    userProviderParams={{
+      db,
+      path: "users/",
+      useDefaultValueForDbDataInProviderWrapper: () => ({
+        displayName: "",
+        email: "",
+        lang: "en",
+        phoneNumber: "",
+        photoURL: "",
+        uid: "",
+      }),
+    }}
+  >
+    <Component {...pageProps} />
+  </Wrapper>
+)
+
+export default appWithTranslation(App)
